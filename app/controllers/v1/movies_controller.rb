7f914cfa894c438d5bdad798d22bdaf62941388a
Nina Yeh <ninayeh1211@gmail.com>
2019-10-24 22:57:25 +0100

    fix

--- app/controllers/v1/movies_controller.rb
+++ app/controllers/v1/movies_controller.rb
@@ -2,6 +2,7 @@ class V1::MoviesController < ApplicationController
   def index
     api_url = ENV['REACT_APP_API_URL']
     api_key = ENV['REACT_APP_API_KEY']
+    puts "API = #{api_url}"
     response = HTTP.get("#{api_url}movie/popular?api_key=#{api_key}&language=en-US&page=1").to_s
     parsed_response = JSON.parse(response)
     results = parsed_response['results']
