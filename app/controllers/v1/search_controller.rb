82b4b32603056d20edff68b7f9d8f17b43993c78
Nina Yeh <ninayeh1211@gmail.com>
2019-10-26 22:21:17 +0100

    update api

--- app/controllers/v1/search_controller.rb
+++ app/controllers/v1/search_controller.rb
@@ -2,14 +2,16 @@ class V1::SearchController < ApplicationController
   def index
     api_url = ENV['REACT_APP_API_URL']
     api_key = ENV['REACT_APP_API_KEY']
-    # searchTerm = "joker"  
-    searchTerm = request.query_parameters['query']
-    puts "searchTerm #{searchTerm}"
-
-    response = HTTP.get("#{api_url}search/movie?api_key=#{api_key}&language=en-US&query=#{searchTerm}").to_s
     
+    searchTerm = request.query_parameters['query']
+    thepage = request.query_parameters['page']
+    
+    response = HTTP.get("#{api_url}search/movie?api_key=#{api_key}&language=en-US&query=#{searchTerm}&page=#{thepage}").to_s
+    therequest = ("#{api_url}search/movie?api_key=#{api_key}&language=en-US&query=#{searchTerm}&page=#{thepage}").to_s
+    puts "therequest = #{therequest}  "
+
     parsed_response = JSON.parse(response)
-    results = parsed_response['results']
+    results = parsed_response
     render json: results
   end
 end
