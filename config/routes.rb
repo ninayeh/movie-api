95ff99c56a11ed05d77dea3c2113d68d106969bb
Nina Yeh <ninayeh1211@gmail.com>
2019-10-26 23:41:07 +0100

    fetch hero image

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :v1, defaults: { format: 'json'} do
-    get 'movies', to: 'movies#index'
-    get 'search', to: 'search#index'
+  # namespace :v1, defaults: { format: 'json'} do
+  namespace :v1  do
+    get 'movies', to: 'movies#index', :format => "json"
+    get 'search', to: 'search#index', :format => "json"
+    get 'images', to: 'images#index', :format => "html"
+
+    
+  
   end
 end
