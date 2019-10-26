aa02337ae5aecacb3d9f2d70b066e43270622a7a
Nina Yeh <ninayeh1211@gmail.com>
2019-10-26 21:28:21 +0100

    fix fetch issue

--- app/controllers/v1/movies_controller.rb
+++ app/controllers/v1/movies_controller.rb
@@ -2,10 +2,9 @@ class V1::MoviesController < ApplicationController
   def index
     api_url = ENV['REACT_APP_API_URL']
     api_key = ENV['REACT_APP_API_KEY']
-    puts "API = #{api_url}"
     response = HTTP.get("#{api_url}movie/popular?api_key=#{api_key}&language=en-US&page=1").to_s
     parsed_response = JSON.parse(response)
-    results = parsed_response['results']
+    results = parsed_response
     render json: results
   end
 end
